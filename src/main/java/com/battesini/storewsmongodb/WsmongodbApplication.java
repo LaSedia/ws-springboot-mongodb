3392c6d3380e8d42787e621aab1372a6122b4b92
LaSedia <leonardoffline@gmail.com>
2019-11-04 23:49:35 -0300

    Criacao do projeto

---
+++ src/main/java/com/battesini/storewsmongodb/WsmongodbApplication.java
@@ -0,0 +1,13 @@
+package com.battesini.storewsmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsmongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsmongodbApplication.class, args);
+	}
+
+}
