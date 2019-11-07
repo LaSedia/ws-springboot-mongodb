a0ae72ae564f2b703c141cc10f694255613c2847
LaSedia <leonardoffline@gmail.com>
2019-11-07 03:58:19 -0200

    Recupera Posts por @Query

--- src/main/java/com/battesini/wsmongodb/repository/PostRepository.java
+++ src/main/java/com/battesini/wsmongodb/repository/PostRepository.java
@@ -1,11 +1,24 @@
 package com.battesini.wsmongodb.repository;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.battesini.wsmongodb.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>	{
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
+
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+
+	@Query("{ $and: [ { date: {$gte: ?1} }, { date: { $lte: ?2} } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+//	("{ $and: [ { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] }, {field: {$gte: ?1} }, {field: {$lte: ?2} ] }")
+	List<Post> fullSearch(String text, Date minDate, Date MaxDate);
 	
 }
--- src/main/java/com/battesini/wsmongodb/resources/PostResource.java
+++ src/main/java/com/battesini/wsmongodb/resources/PostResource.java
@@ -1,13 +1,19 @@
 package com.battesini.wsmongodb.resources;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.battesini.wsmongodb.domain.Post;
+import com.battesini.wsmongodb.resources.util.URL;
 import com.battesini.wsmongodb.services.PostService;
 
 @RestController
@@ -21,6 +27,24 @@ public class PostResource {
 	public ResponseEntity<Post> findById(@PathVariable String id)	{
 		Post post = service.findById(id);
 		return ResponseEntity.ok().body(post);
+	}
+	
+	@GetMapping(value="/titlesearch")
+	public ResponseEntity<List <Post>> fullSearch(@RequestParam(value = "text", defaultValue = "") String text)	{
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value="/fullsearch")
+	public ResponseEntity<List <Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text,
+			@RequestParam(value = "maxDate", defaultValue = "") String maxDate,
+			@RequestParam(value = "minDate", defaultValue = "") String minDate)	{
+		text = URL.decodeParam(text);
+		Date max = URL.convertDate(maxDate, new Date()); //Default HOJE
+		Date min = URL.convertDate(minDate, new Date(0L)); //Default 1970
+		List<Post> list = service.fullSearch(text, min, max);
+		return ResponseEntity.ok().body(list);
 	}
 	
 }
